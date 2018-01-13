bf53bb386014247c4c03cc0586996bfd193e2651
oilnur <ilnursoft@gmail.com>
2018-01-13 18:49:17 +0300

    добавлена возможность парсинга всех страниц сайта цитат

--- firstscrapyprj/firstscrapyprj/spiders/quotes_spider.py
+++ firstscrapyprj/firstscrapyprj/spiders/quotes_spider.py
@@ -1,14 +1,18 @@
 import scrapy
 from ..items import FirstscrapyprjItem
-from scrapy.selector import HtmlXPathSelector
-from scrapy.contrib.loader.processor import TakeFirst
-from scrapy.contrib.loader import XPathItemLoader
+from scrapy.spiders import Rule
+from scrapy.linkextractors import LinkExtractor
 
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-    #allowed_domains = ["quotes.toscrape.com"]
-   
+    allowed_domains = ["quotes.toscrape.com"]    
+    """
+    start_urls = [
+        "http://quotes.toscrape.com/page/1/"
+    ]   
+  
+    """
     def start_requests(self):
         urls = [
                 "http://quotes.toscrape.com/page/1/",
@@ -17,15 +21,20 @@ class QuotesSpider(scrapy.Spider):
         items=[]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
-            
-        #return items
-   
 
     def parse(self,response):
         item = FirstscrapyprjItem()
         for quote in response.xpath('//div[@class="quote"]'):            
             item["text_quote"]=quote.xpath("span[contains(@class,'text')]/text()").extract()
             item["author"]=quote.xpath("*/small[contains(@class,'author')]/text()").extract()
-            item["tags"]= "".join(quote.xpath("div[contains(@class,'tags')]/meta/@content").extract())            
+            item["tags"]= "".join(quote.xpath("div[contains(@class,'tags')]/meta/@content").extract())                        
             yield item
+        # опредление урла следующей страницы     
+        next_page = response.xpath('//li[@class="next"]/a/@href').extract_first()
+        print("Страница = ", next_page)
+        if next_page is not None:
+             # обработка следующей страницы
+             yield scrapy.Request(response.urljoin(next_page))
+
+        
 
